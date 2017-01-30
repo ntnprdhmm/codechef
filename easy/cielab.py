0e8bf6d40554fd09ab42ab42560404c6061763ad
prudywsh <antoineprudhomme5@gmail.com>
2017-01-30 15:44:28 +0100

    ok the previous solution for cielab was insane, I found an infinitely better way to do it

--- easy/cielab.py
+++ easy/cielab.py
@@ -1,14 +1,7 @@
 a, b = map(int, input().split())
 
 r = a - b
-splitted_r = [int(i) for i in str(r)]
-
-for idx in range(len(splitted_r)):
-    if splitted_r[idx] > 1:
-        splitted_r[idx] = splitted_r[idx] - 1
-        break
-    elif splitted_r[idx] < 9:
-        splitted_r[idx] = splitted_r[idx] + 1
-        break
-
-print(''.join(str(i) for i in splitted_r))
+if r % 10 == 9:
+    print(r-1)
+else:
+    print(r+1)
